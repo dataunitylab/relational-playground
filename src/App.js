3f357858a637f9f95ce51018ba0337686b85bb1c
Michael Mior <mmior@cs.rit.edu>
2018-10-10 19:40:56 -0400

    Integrate redux and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <main>
+        </main>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+import { ConnectedRouter } from 'connected-react-router'
+import { Provider } from 'react-redux'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import store, { history } from './store'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
