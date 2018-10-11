26866f73818fb8ca2d393c42b36e0591639716da
Michael Mior <mmior@cs.rit.edu>
2018-10-10 22:01:30 -0400

    Add simple unary relational algebra operators

---
+++ src/RelOp.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class RelOp extends Component {
+  render() {
+    return (<span>{this.props.operator}({this.props.children})</span>);
+  }
+}
+
+class Projection extends Component {
+  render() {
+    return <span>&pi;<sub>{this.props.project.join(',')}</sub></span>;
+  }
+}
+
+class Rename extends Component {
+  render() {
+    return <span>&rho;<sub>{Object.entries(this.props.rename).map(([o, n]) => o + '/' + n)}</sub></span>;
+  }
+}
+
+class Selection extends Component {
+  render() {
+    return <span>&sigma;<sub>{this.props.select.join(',')}</sub></span>;
+  }
+}
+
+export { RelOp as default, Projection, Rename, Selection };
--- src/stories/index.js
+++ src/stories/index.js
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import RelOp, { Projection, Rename, Selection } from '../RelOp';
 import Table from '../Table';
 
 storiesOf('Table', module)
@@ -17,4 +17,22 @@ storiesOf('Table', module)
         {firstName: "Bob", lastName: "Smith", salary: 120000},
       ]}>
     </Table>
+  ));
+
+storiesOf('RelOp', module)
+  .add('a simple projection', () => (
+    <RelOp operator={<Projection project={['firstName', 'lastName']}/>}>Doctor</RelOp>
+  ))
+  .add('a simple selection', () => (
+    <RelOp operator={<Selection select={['salary > 100K']}/>}>Doctor</RelOp>
+  ))
+  .add('a simple rename', () => (
+    <RelOp operator={<Rename rename={{'firstName': 'name'}}/>}>Doctor</RelOp>
+  ))
+  .add('nested operations', () => (
+    <RelOp operator={<Rename rename={{'firstName': 'name'}}/>}>
+      <RelOp operator={<Projection project={['firstName', 'lastName']}/>}>
+        <RelOp operator={<Selection select={['salary > 100K']}/>}>Doctor</RelOp>
+      </RelOp>
+    </RelOp>
   ));
