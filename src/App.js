b541f4bd3300e1bef56b909fe0a14835c03c23d3
Michael Mior <mmior@cs.rit.edu>
2018-11-15 09:01:17 -0500

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
 
