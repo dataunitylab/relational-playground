109263cf365a982eb6232f8a35b506edf322c420
Michael Mior <mmior@cs.rit.edu>
2019-05-27 12:15:54 -0400

    Add simple test for rendering without crashing

---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {MemoryRouter} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  const store = createStore(() => ({
+    relexp: {expr: {}},
+    data: {sourcedata: {}},
+  }));
+  ReactDOM.render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
