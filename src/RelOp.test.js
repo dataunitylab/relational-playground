fa6c7fdd1ed4429d52afc2af482d1120e8c99f34
Michael Mior <mmior@cs.rit.edu>
2019-09-12 15:47:37 -0400

    Add binary RelOp tests

--- src/RelOp.test.js
+++ src/RelOp.test.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 
-import {Projection, Rename, Selection} from './RelOp';
+import {
+  Projection,
+  Rename,
+  Selection,
+  Except,
+  Intersect,
+  Join,
+  Union,
+} from './RelOp';
 
 /** @test {Projection} */
 it('renders a projection', () => {
@@ -31,4 +39,28 @@ it('renders a Selection with single predicate', () => {
 it('renders a Selection with multiple predicates', () => {
   const {container} = render(<Selection select={['foo=3', 'bar=2']} />);
   expect(container).toContainHTML('σ<sub>foo=3 ∧ bar=2</sub>');
+});
+
+/** @test {Except} */
+it('renders a Except', () => {
+  const {container} = render(<Except />);
+  expect(container).toContainHTML('−');
+});
+
+/** @test {Intersect} */
+it('renders a Intersect', () => {
+  const {container} = render(<Intersect />);
+  expect(container).toContainHTML('∩');
+});
+
+/** @test {Join} */
+it('renders a Join', () => {
+  const {container} = render(<Join />);
+  expect(container).toContainHTML('×');
+});
+
+/** @test {Union} */
+it('renders a Union', () => {
+  const {container} = render(<Union />);
+  expect(container).toContainHTML('∪');
 });
