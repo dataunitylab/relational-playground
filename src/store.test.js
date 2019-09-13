0862b251a0cd5d146a22648399e29ee1f68c6f45
Michael Mior <mmior@cs.rit.edu>
2019-09-13 10:35:52 -0400

    Add test for dev tools initialization

--- src/store.test.js
+++ src/store.test.js
@@ -1,9 +1,30 @@
-import store from './store';
-console.log(store);
+import {createStore, applyMiddleware, compose} from 'redux';
+
+const OLD_ENV = process.env;
+
+beforeEach(() => {
+  jest.resetModules(); // this is important - it clears the cache
+  process.env = {...OLD_ENV};
+  delete process.env.NODE_ENV;
+});
 
 it('has a valid initial state', () => {
+  const store = require('./store').default;
   const initialState = store.getState();
   expect(initialState).toHaveProperty('data');
   expect(initialState).toHaveProperty('relexp');
   expect(initialState).toHaveProperty('router');
+});
+
+it('initializes dev tools in development', () => {
+  process.env.NODE_ENV = 'development';
+
+  // Pretend the dev tools are installed
+  const devTools = jest.fn((...args) => applyMiddleware(...args));
+  global.__REDUX_DEVTOOLS_EXTENSION__ = devTools;
+
+  const store = require('./store').default;
+  expect(devTools.mock.calls.length).toBe(1);
+
+  process.env.NODE_ENV = 'test';
 });
