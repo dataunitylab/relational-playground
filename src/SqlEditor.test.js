54eebb790a4cc98dbd92bf815a733ed784c4e7b4
Carson Bloomingdale <ceb3745@g.rit.edu>
2019-10-16 14:44:03 -0400

    updated sqleditor test

--- src/SqlEditor.test.js
+++ src/SqlEditor.test.js
@@ -7,12 +7,14 @@ it('can parse the initial query', () => {
   const types = {foo: ['bar', 'baz']};
   const mockAction = jest.fn();
   const mockEvent = jest.fn();
+  const mockChangeAction = jest.fn(() => undefined);
   const wrapper = shallow(
     <SqlEditor
       defaultText="SELECT * FROM foo"
       ReactGA={{event: mockEvent}}
       exprFromSql={mockAction}
       types={types}
+      changeAction={mockChangeAction}
     />
   );
 
@@ -23,6 +25,7 @@ it('can parse the initial query', () => {
 
   // No events should be recorded
   expect(mockEvent.mock.calls.length).toBe(0);
+  expect(mockChangeAction.mock.calls.length).toBe(0);
 });
 
 it('can parse modified query and fire an event', () => {
@@ -30,12 +33,14 @@ it('can parse modified query and fire an event', () => {
   const types = {foo: ['bar', 'baz']};
   const mockAction = jest.fn(() => undefined);
   const mockEvent = jest.fn(() => undefined);
+  const mockChangeAction = jest.fn(() => undefined);
   const wrapper = shallow(
     <SqlEditor
       defaultText="SELECT * FROM foo"
       ReactGA={{event: mockEvent}}
       exprFromSql={mockAction}
       types={types}
+      changeAction={mockChangeAction}
     />
   );
 
@@ -55,6 +60,7 @@ it('can parse modified query and fire an event', () => {
 
   // Now an event should be fire with the new query and an action created
   expect(mockEvent.mock.calls.length).toBe(1);
+  expect(mockChangeAction.mock.calls.length).toBe(1);
   expect(mockEvent.mock.calls[0][0].category).toBe('User Typing SQL Statement');
   expect(mockEvent.mock.calls[0][0].action).toBe(query);
   expect(mockAction.mock.calls.length).toBe(2);
