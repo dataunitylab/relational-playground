8b01a410f0241492e47e8420365aa3ec1eeb0bd0
Carson Bloomingdale <ceb3745@g.rit.edu>
2020-02-11 11:48:03 -0500

    Tutorial on first load

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as Sentry from '@sentry/browser';
 import './index.css';
 import store, {history} from './store';
 import App from './App';
+import CookiesProvider from 'react-cookie/cjs/CookiesProvider';
 
 let sentryConfig = {};
 sentryConfig.dsn = 'https://496d372d00a24c57af7967ac2ff5dacd@sentry.io/2445886';
@@ -18,12 +19,14 @@ if (process.env.REACT_APP_GIT_SHA) {
 Sentry.init(sentryConfig);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
-    </ConnectedRouter>
-  </Provider>,
+  <CookiesProvider>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div>
+          <App />
+        </div>
+      </ConnectedRouter>
+    </Provider>
+  </CookiesProvider>,
   ((document.getElementById('root'): any): HTMLElement)
 );
