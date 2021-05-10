bc12f9ac13803bec0bdd22fd8fe8a28e95cf6528
Michael Mior <mmior@cs.rit.edu>
2021-05-10 19:46:47 -0400

    Bump React packages from 16.14.0 to 17.0.2

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-enzyme';
 import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({adapter: new Adapter()});
 
