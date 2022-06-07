12bcc0d2a4a53e51dbf415612aacc8518eb70931
Michael Mior <mmior@cs.rit.edu>
2022-06-07 02:47:04 -0400

    Temporarily remove outdated flow-typed definitions

--- flow-typed/npm/deep-freeze_v0.0.1.js
+++
@@ -1,6 +0,0 @@
-// flow-typed signature: dd48a9b9eb482a63ddb9915bdeefacee
-// flow-typed version: c6154227d1/deep-freeze_v0.0.1/flow_>=v0.104.x
-
-declare module 'deep-freeze' {
-  declare module.exports: <T>(o: T) => T;
-}
