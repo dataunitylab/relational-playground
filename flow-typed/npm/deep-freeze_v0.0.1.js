33708bda6f3237d93d0c7da5b44a5b251f18da7d
michaelmior <michaelmior@users.noreply.github.com>
2022-07-17 03:33:28 +0000

    Update flow-typed definitions

--- flow-typed/npm/deep-freeze_v0.0.1.js
+++ flow-typed/npm/deep-freeze_v0.0.1.js
@@ -1,6 +1,19 @@
-// flow-typed signature: dd48a9b9eb482a63ddb9915bdeefacee
-// flow-typed version: c6154227d1/deep-freeze_v0.0.1/flow_>=v0.104.x
+// flow-typed signature: eb61173a8bf860d5fdf5447b6b1352cc
+// flow-typed version: 3793c65651/deep-freeze_v0.0.1/flow_>=v0.104.x
 
 declare module 'deep-freeze' {
-  declare module.exports: <T>(o: T) => T;
+  declare type deepFreezeFnType = {|
+    <T>(a: $ReadOnlyArray<T>): $ReadOnlyArray<DeepReadOnly<T>>,
+    <T>(a: T[]): $ReadOnlyArray<DeepReadOnly<T>>,
+    <T: boolean|string|number>(p: T): T,
+    <T>(o: T): $ReadOnly<
+      $ObjMapi<T, <P>(P) => DeepReadOnly<$ElementType<T, P>>>
+    >,
+    <T: Function>(f: T): T,
+  |};
+
+  declare export type DeepReadOnly<T> = $Call<deepFreezeFnType, T>
+
+  declare module.exports: deepFreezeFnType;
+
 }
