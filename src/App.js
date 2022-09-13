1769110a88932f559e6628eec9062ed803c7e3c8
aryanjha <axj2613@rit.edu>
2022-09-13 05:53:09 -0400

    Implemented dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import Home from './Home';
+import useDarkMode from 'use-dark-mode';
+
 
 /** A container for all routes in the app (currently only one) */
 function App() {
-  return (
+
+    const darkMode = useDarkMode(false);
+
+    return (
     <div className="App">
-      <main>
-        <Route path={process.env.PUBLIC_URL} component={Home} />
-      </main>
+        <div className={darkMode.value ? "dark" : "light"}>
+            <main>
+                <Route path={process.env.PUBLIC_URL} component={Home} />
+            </main>
+        </div>
     </div>
   );
 }
