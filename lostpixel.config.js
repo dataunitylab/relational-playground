98c9ce941201abf04be1fd03894e678cdd5d1a37
Michael Mior <mmior@mail.rit.edu>
2023-01-17 08:03:59 -0500

    Try Lost Pixel

---
+++ lostpixel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  storybookShots: {
+    storybookUrl: './storybook-static',
+  },
+  lostPixelProjectId: 'cld08suck0455ke0vt3werogj',
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
