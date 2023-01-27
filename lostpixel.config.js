ea1ce8c92e9697a1e37919e4a5d4d273c21104f5
Michael Mior <mmior@mail.rit.edu>
2023-01-27 09:03:52 -0500

    Revert "Try Lost Pixel"
    This reverts commit 98c9ce941201abf04be1fd03894e678cdd5d1a37.

--- lostpixel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  storybookShots: {
-    storybookUrl: './storybook-static',
-  },
-  lostPixelProjectId: 'cld08suck0455ke0vt3werogj',
-  apiKey: process.env.LOST_PIXEL_API_KEY,
-};
