c50b13236fdcb98d756a422f73460439b2052c9d
Michael Mior <mmior@mail.rit.edu>
2023-04-11 09:12:42 -0400

    Upgrade to Storybook v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,17 @@
-module.exports = {
-  "core": {
-    "builder": "webpack5",
-    "options": {
-      "lazyCompilation": true,
+/** @type { import('@storybook/react).StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      lazyCompilation: true,
     },
   },
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react"
-}
+};
+export default config;
