758a0d28f28d68160728723027421340859b744a
Michael Mior <mmior@mail.rit.edu>
2023-11-02 09:08:42 -0400

    Fix highlighting for stories

--- src/RelExpr.stories.js
+++ src/RelExpr.stories.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {produce} from 'immer';
+import {configureStore} from '@reduxjs/toolkit';
 
 import RelExpr from './RelExpr';
+
+const initialState = {data: {expr: null}};
+const mockStore = configureStore({
+  reducer: {
+    data: produce((state, action) => state, initialState),
+  },
+  preloadedState: initialState,
+});
+
+const MockedStore = ({children}) => (
+  <Provider store={mockStore}>{children}</Provider>
+);
 
 const RelExprStories = {
   title: 'RelExpr',
   component: RelExpr,
+  decorators: [(story) => <MockedStore>{story()}</MockedStore>],
 };
 
 export default RelExprStories;
