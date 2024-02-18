23b925fadf6f48e1cc4e41a76686d2984c543549
michaelmior <michaelmior@users.noreply.github.com>
2024-02-18 02:09:04 +0000

    Update flow-typed definitions

--- flow-typed/npm/react-dom_v18.x.x.js
+++ flow-typed/npm/react-dom_v18.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 1d4beeed8c995b17afd06f505b9e5e25
-// flow-typed version: 7a3d9c1b8b/react-dom_v18.x.x/flow_>=v0.127.x
+// flow-typed signature: bd8a9984746306d26194a489f3aeff35
+// flow-typed version: 388e9edcf0/react-dom_v18.x.x/flow_>=v0.127.x
 
 declare module 'react-dom_shared-types' {
   /**
@@ -129,6 +129,8 @@ declare module 'react-dom' {
   ): React$Portal;
 
   declare function unmountComponentAtNode(container: any): boolean;
+
+  declare function flushSync(callback: () => mixed): void;
 
   declare function unstable_batchedUpdates<A, B, C, D, E>(
     callback: (a: A, b: B, c: C, d: D, e: E) => mixed,
