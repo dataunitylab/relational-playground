6ecada8992d4b0c065e8203ea667f9f76595897e
Michael Mior <mmior@mail.rit.edu>
2025-12-20 21:27:07 -0500

    Update related Storybook packages
    Signed-off-by: Michael Mior <mmior@mail.rit.edu>

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,6 @@ const config = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
     '@chromatic-com/storybook',
   ],
